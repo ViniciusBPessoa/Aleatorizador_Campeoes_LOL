887a1c32faeca476571d1369955f2ef553138022
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-11-06 23:59:43 -0300

    tentativa de fazer ele printar as imagens dos campioes

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 import random
 import requests
 import json
+import os
+from PIL import Image
+from img2txt import Img2Txt
 
 url_champ = "https://ddragon.leagueoflegends.com/cdn/10.22.1/data/pt_BR/champion.json"
 url_spells = "https://ddragon.leagueoflegends.com/cdn/10.22.1/data/pt_BR/summoner.json"
+champion_thumbnails_folder = "champion_thumbnails"
 
-def select(n_players = 5, funk = False):
+# ... (código anterior permanece igual)
 
+def display_image_in_terminal(champion_name):
+    champion_image_path = os.path.join(champion_thumbnails_folder, f"{champion_name}.png")
+
+    if os.path.exists(champion_image_path):
+        with open(champion_image_path, 'rb') as f:
+            img = Image.open(f)
+            img2txt = Img2Txt(img)
+            img2txt.display()
+
+def select(n_players=5):
     shuffle_list = random.sample(posi_list, len(posi_list))
-
-    champion_random = random.sample(champion_list,n_players)
+    champion_random = random.sample(champion_list, n_players)
 
     for i in range(len(champion_random)):
-        
         spell_random = random.sample(spell_list, 2)
+
+        champion_name = champion_random[i]
+        display_image_in_terminal(champion_name)
 
         if shuffle_list[i] == "Jg":
             if "Golpear" not in spell_random:
                 spell_random[1] = "Golpear"
-            print("Posição: Jg, Champion: ", champion_random[i])
-            print("Summoner Spells: ", spell_random)
-            print(f"Sua bild sera: {random.choice(list_bilds)}")
+            print(f"Posição: Jg, Champion: {champion_name}")
+            print(f"Sua build será: {random.choice(list_bilds)}")
         else:
             pos = shuffle_list[i]
-            print(f"Posição: {pos}, Champion: ", champion_random[i])
-            print("Summoner Spells: ", spell_random)
-            print(f"Sua bild sera: {random.choice(list_bilds)}")
+            print(f"Posição: {pos}, Champion: {champion_name}")
+            print(f"Sua build será: {random.choice(list_bilds)}")
 
+        print("Summoner Spells:", spell_random)
+        print("-" * 50)
 
-        print("-"*50)
+    input("Pressione Enter para continuar...")  # Aguarda a entrada do usuário antes de fechar a imagem
 
 response_champ = requests.get(url_champ)
 data_champ = json.loads(response_champ.text)
